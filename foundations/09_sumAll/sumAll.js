4082ed5853c56b6457ba338f52c29df641c95ec5
Brayden Curtis <br@ydencurt.is>
2026-03-25 18:35:44 -0700

    Sort arguments and get sum of all numbers between them

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    const sorted = [int1, int2];
+    sorted.sort(function(a,b){return a-b});
+    let sum = 0;
+    for (i = sorted[0]; i <= sorted[1]; i++) {
+        sum = sum + i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
