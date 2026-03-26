474a2989eaee0c884cb57c7795cab2ab3c70b8c0
Brayden Curtis <br@ydencurt.is>
2026-03-25 18:38:58 -0700

    Error if negative number in argument

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,8 @@
 const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    
     const sorted = [int1, int2];
     sorted.sort(function(a,b){return a-b});
     let sum = 0;
