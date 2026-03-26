be6264404406a3e8060f3d3b217c6a341e1011dd
Brayden Curtis <br@ydencurt.is>
2026-03-25 18:42:49 -0700

    Check if args are integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(int1, int2) {
-    if (int1 < 0 || int2 < 0) {
+    if (int1 < 0 || int2 < 0 || !Number.isInteger(int1) || !Number.isInteger(int2)) {
         return 'ERROR';
     }
     
