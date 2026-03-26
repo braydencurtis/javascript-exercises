17b99e974f66e33398c92fe59fadfc600c3b898a
Brayden Curtis <br@ydencurt.is>
2026-03-25 19:48:12 -0700

    Convert temp from Farenheit to Celsius and round to 1 decimal point.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp) {
+  const tempInCelsius = (temp - 32) * (5/9);
+  return Math.round(tempInCelsius * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
 };
 
 // Do not edit below this line
